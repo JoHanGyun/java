3054eaa89c4eb17a88d0dc30d47ad17aefb85f04
JoHanGyun <109188612+JoHanGyun@users.noreply.github.com>
2023-03-15 10:42:26 +0900

    자바 언어

---
+++ CadtingExam2.java
@@ -0,0 +1,11 @@
+package datatype;
+
+public class CadtingExam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		byte b = (byte)a;
+	}
+
+}
---
+++ DataTypeExam1.java
@@ -0,0 +1,11 @@
+package datatype;
+
+public class DataTypeExam1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=214748348;
+		System.out.println(a);
+	}
+
+}
